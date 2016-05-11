ca2baa088a5bf8da22a4f140c966f4057c506618
Wayne Rumble <wrumble@Waynes-MacBook-Pro.local>
2016-05-11 16:44:16 +0100

    finsihed jquery

--- src/script.js
+++ src/script.js
@@ -1,13 +1,48 @@
-var thermostat = new Thermostat;
+var thermostat = new Thermostat();
 
 $(document).ready(function() {
+
+  displayTemp();
+  PSMStatus();
+  updateColour();
+
   $('#temp-up').click(function(){
     thermostat.increaseTemp();
+    displayTemp();
+    updateColour();
   });
-});
 
-$(document).ready(function() {
   $('#temp-down').click(function(){
     thermostat.decreaseTemp();
+    displayTemp();
+    updateColour();
   });
+
+  $('#temp-reset').click(function() {
+    thermostat.reset();
+    displayTemp();
+  });
+
+  $('#psm-toggle').click(function() {
+    thermostat.togglePSM();
+    PSMStatus();
+  });
+
 });
+
+var displayTemp = function() {
+  $('h2').text(thermostat.temperature());
+};
+
+var PSMStatus = function() {
+  $('p').text(function() {
+    if(thermostat.isPSMOn()) {
+      return "Power Saving Mode: On";
+    }
+    return "Power Saving Mode: Off";
+  });
+};
+
+var updateColour = function() {
+  $('#display-panel').css("background-color", thermostat.display());
+};
