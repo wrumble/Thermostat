600bbebe313bbcc9b0d9acef2f6258fd1298022d
Wayne Rumble <wrumble@Waynes-MacBook-Pro.local>
2016-05-11 10:54:29 +0100

    power saving mode and min temp tests pass

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -14,14 +14,24 @@ describe("Thermostat", function() {
     for(i = 1; i < 5; i++) {
       thermostat.increaseTemp();
       expect(thermostat.temperature()).toEqual(thermostat.DEFAULT_TEMP + i);
-    };
+    }
   });
 
-    it('can decrease the temperature', function() {
+  it('can decrease the temperature', function() {
     for(i = 1; i < 5; i++) {
       thermostat.decreaseTemp();
       expect(thermostat.temperature()).toEqual(thermostat.DEFAULT_TEMP - i);
-    };
+    }
+  });
+
+  it('has a minimum temperature', function() {
+    thermostat._temperature = thermostat.MIN_TEMP;
+    thermostat.decreaseTemp();
+    expect(thermostat.temperature()).toEqual(thermostat.MIN_TEMP);
+  });
+
+  it('starts in power saving mode', function() {
+    expect(thermostat.isPSMOn()).toEqual(true);
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,6 @@
 function Thermostat(){
   this.DEFAULT_TEMP = 20;
+  this.MIN_TEMP = 10;
   this._temperature = this.DEFAULT_TEMP;
 
   this.temperature = function() {
@@ -7,10 +8,16 @@ function Thermostat(){
   };
 
   this.increaseTemp = function() {
-    this._temperature ++
-  }
+    this._temperature ++;
+  };
 
   this.decreaseTemp = function() {
-    this._temperature --
-  }
+    if(this._temperature > this.MIN_TEMP) {
+      this._temperature --;
+    }
+  };
+
+  this.isPSMOn = function() {
+    return true;
+  };
 }
